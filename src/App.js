e54291b9ccaac25347b2c24b0a75c3f64482c5cb
Anshul <anshulporwal2001@gmail.com>
2023-10-07 00:03:54 +0530

    Added components, react-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
-      <h1>This is iNotebook</h1>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/about" element={<About />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
