cfdc88a541126ccf760a958074e901391267a961
Anshul <anshulporwal2001@gmail.com>
2023-10-07 17:42:52 +0530

    Added delete functionality w/o any alert

--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -1,17 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Noteitem(props) {
     const { note } = props;
 
+    const [noteDescription, setnoteDescription] = useState(note.description)
+    const deleteNote = ()=>{
+       setnoteDescription("")
+    }
     return (
         <div className='col-md-3 my-3'> 
             <div className="card">
                 <div className="card-body">
                     <h5 className="card-title">{note.title}</h5>
-                    <p className="card-text">{note.description}</p>
+                    <p className="card-text">{noteDescription}</p>
                     <div className="d-flex">
                         <i className="fa-regular fa-pen-to-square"></i>
-                        <i className="fa-regular fa-trash-can mx-2"></i>
+                        <i className="fa-regular fa-trash-can mx-2" onClick={deleteNote}></i>
                     </div>
                     
                 </div>
